29d292c9f42484b8ebb5b18f139b084b49fe51c2
Evelina <evelina.evish@yandex.ru>
2022-04-30 10:12:04 +0300

    Доработала Example_2

--- Example_2/Program.cs
+++ Example_2/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Написать программу, запрашивающуу имя пользователя и выводящая затем на экран "Привет, имя пользователя"
 
 
-Console.WriteLine("Введите ваше имя");
+Console.WriteLine("Введите ваше имя ");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.WriteLine(username);
+
+
+
