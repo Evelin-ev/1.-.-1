abeeb2e9055a843d85d17b8ac14388bb47c893d6
Evelina <evelina.evish@yandex.ru>
2022-04-30 09:28:25 +0300

    Добавила папку Example_2 и файл .gitignore

---
+++ Example_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу, запрашивающуу имя пользователя и выводящая затем на экран "Привет, имя пользователя"
+
+
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
