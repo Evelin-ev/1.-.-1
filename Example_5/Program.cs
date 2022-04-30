46537da68a5b73233ed190f7e2daa87c9cfd431b
Evelina <evelina.evish@yandex.ru>
2022-04-30 10:55:52 +0300

    Добавила папку Example_5, создала программу сложения 2 рандомных чисел

---
+++ Example_5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа для сложения двух рандомных чисел
+
+int numberA = new Random().Next(1, 100);
+Console.WriteLine(new Random().Next(1, 100));
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(new Random().Next(1, 100));
+int result = numberA + numberB;
+Console.WriteLine(result);
