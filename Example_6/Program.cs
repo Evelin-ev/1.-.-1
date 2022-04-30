6577dbd7e9ff80f9936653da89692e0114a9927e
Evelina <evelina.evish@yandex.ru>
2022-04-30 11:55:48 +0300

    Добавила папку Example_6 и написала пожелание под ещё одно имя

--- Example_6/Program.cs
+++ Example_6/Program.cs
@@ -7,6 +7,36 @@ if(username.ToLower() == "эвелина")
 {
     Console.WriteLine("Привет, красотка! Всё будет хорошо! Даже если сейчас ты в этом не уверена:)");
 }
+
+if(username.ToLower() == "саня")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+
+if(username.ToLower() == "александра")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+
+if(username.ToLower() == "санек")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+
+if(username.ToLower() == "санёк")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+
+if(username.ToLower() == "сашка")
+{
+    Console.WriteLine("Привет, бро! Всё будет хорошо! Ты крутая!");
+}
+
 else
 {
     Console.Write("Привет, ");
