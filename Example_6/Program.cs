de54a97aee4cd2927d2cdcfbc899b9227e54e4dc
Evelina <evelina.evish@yandex.ru>
2022-04-30 11:18:11 +0300

    Добавила папку Example_6 и написала пожелание под свое имя

---
+++ Example_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, по-другому реагирующая на конкретное имя
+
+Console.WriteLine("Введите ваше имя: ");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "эвелина")
+{
+    Console.WriteLine("Привет, красотка! Всё будет хорошо! Даже если сейчас ты в этом не уверена:)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
